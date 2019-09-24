21107703f239b045371b999300e7e0dec8e2cd97
Da Vinci <jeb2je@gmail.com>
2019-09-24 12:16:02 -0400

    Removed key

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,5 @@
 var config = {
-    apiKey: "AIzaSyBKZOHrc4JYi5EG0wBu6815k38Y_HpsxKo",
-    authDomain: "fir-1-eaeaf.firebaseapp.com",
-    databaseURL: "https://fir-1-eaeaf.firebaseio.com",
-    projectId: "fir-1-eaeaf",
-    storageBucket: "",
-    messagingSenderId: "247460823980",
-    appId: "1:247460823980:web:9b0a2b8102321c6dd67009"
+    
   };
 
 
